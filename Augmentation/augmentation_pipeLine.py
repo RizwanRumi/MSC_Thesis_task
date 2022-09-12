e7ba8f037eab1c55ffa2e51064d00bd04762a1ac
RizwanRumi <rizwan.cse33@gmail.com>
2022-09-12 20:59:21 +0200

    update and redesigned augmented functions

--- Augmentation/augmentation_pipeLine.py
+++ Augmentation/augmentation_pipeLine.py
@@ -1,10 +1,55 @@
 import os
 import glob
 import cv2
+import numpy as np
 import imgaug as ia 
 import imgaug.augmenters as iaa
 from pathlib import Path
 import matplotlib.pyplot as plt
+
+def color_and_negative(image):
+    negative_list = []
+    title_list = []
+    
+    # color, gray 
+    color_img = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    negative_list.append(np.asarray(color_img))
+    title_list.append("colored")
+    
+    gray_img = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    negative_list.append(np.asarray(gray_img))
+    title_list.append("gray")
+    
+    #colored negative and gray negative
+    colored_negative = abs(255 - color_img)
+    negative_list.append(np.asarray(colored_negative))
+    title_list.append("colr_neg")
+    
+    gray_negative = abs(255 - gray_img)
+    negative_list.append(np.asarray(gray_negative))
+    title_list.append("gray_neg")
+    
+    return negative_list, title_list
+
+
+def flip(image):
+    flip_list = []
+    title_list = []
+    
+    #Horizontal Flip
+    hflip = iaa.Fliplr(p=1.0)
+    hf_img = hflip.augment_image(image)
+    flip_list.append(np.asarray(hf_img))
+    title_list.append("h_flip")
+    
+    #Vertical Flip
+    vflip = iaa.Flipud(p=1.0)
+    vf_img = vflip.augment_image(image)
+    flip_list.append(np.asarray(vf_img))
+    title_list.append("v_flip")
+    
+    return flip_list, title_list
+
 
 imdir = 'images/'
 ext = ['jpg', 'jpeg']   
@@ -16,30 +61,21 @@ images = [cv2.imread(file) for file in files]
           
 total_image = len(images)
 
+aug_img_list = []
+labels = []
+
 for i in range(total_image):
     
-    # color, gray 
-    color_img = cv2.cvtColor(images[i], cv2.COLOR_BGR2RGB)
+    # negative image augmentation
+    negative_res, titles = color_and_negative(images[i])
+    aug_img_list = negative_res
+    labels = titles
     
-    gray_img = cv2.cvtColor(images[i], cv2.COLOR_BGR2GRAY)
+    # flip augmentation
+    flip_res, titles = flip(images[i])
+    aug_img_list.extend(flip_res)
+    labels.extend(titles)
     
-    #colored negative and gray negative
-    colored_negative = abs(255 - color_img)
-    gray_negative = abs(255 - gray_img)
-    
-    #Horizontal Flip
-    hflip = iaa.Fliplr(p=1.0)
-    hf_img = hflip.augment_image(images[i])
-
-    #Vertical Flip
-    vflip = iaa.Flipud(p=1.0)
-    vf_img = vflip.augment_image(images[i])
-            
-    aug_img_list = [color_img, gray_img, colored_negative, gray_negative,
-                    hf_img, vf_img]
-    
-    labels = ["colored", "gray", "color_neg", "gray_neg",
-              "hor_flip", "ver_flip"]
     
     img_name = Path(files[i]).stem
         
@@ -54,3 +90,5 @@ for i in range(total_image):
         plt.yticks([])
         
     plt.show()
+    aug_img_list = []
+    labels = []
