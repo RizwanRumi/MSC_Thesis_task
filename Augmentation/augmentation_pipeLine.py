267bd79ada672985b27df51d601ebe0fd8bd8130
RizwanRumi <rizwan.cse33@gmail.com>
2022-09-12 18:51:02 +0200

    add horizontal flip, vertical flip method

--- Augmentation/augmentation_pipeLine.py
+++ Augmentation/augmentation_pipeLine.py
@@ -1,6 +1,8 @@
 import os
 import glob
 import cv2
+import imgaug as ia 
+import imgaug.augmenters as iaa
 from pathlib import Path
 import matplotlib.pyplot as plt
 
@@ -24,10 +26,20 @@ for i in range(total_image):
     #colored negative and gray negative
     colored_negative = abs(255 - color_img)
     gray_negative = abs(255 - gray_img)
-            
-    aug_img_list = [color_img, gray_img, colored_negative, gray_negative]
     
-    labels = ["colored", "gray", "colored_negative", "gray_negative"]
+    #Horizontal Flip
+    hflip = iaa.Fliplr(p=1.0)
+    hf_img = hflip.augment_image(images[i])
+
+    #Vertical Flip
+    vflip = iaa.Flipud(p=1.0)
+    vf_img = vflip.augment_image(images[i])
+            
+    aug_img_list = [color_img, gray_img, colored_negative, gray_negative,
+                    hf_img, vf_img]
+    
+    labels = ["colored", "gray", "color_neg", "gray_neg",
+              "hor_flip", "ver_flip"]
     
     img_name = Path(files[i]).stem
         
@@ -35,11 +47,10 @@ for i in range(total_image):
         
         #isWritten = cv2.imwrite('E:/Thesis_Task/Augmentation/augmented_images/'+img_name+'_'+labels[j]+'.jpg', aug_img_list[j])
         
-        plt.subplot(1, 4, j+1)
+        plt.subplot(1, 6, j+1)
         plt.title(labels[j])
         plt.imshow(aug_img_list[j])
         plt.xticks([])
         plt.yticks([])
         
     plt.show()
-    #print("\n")
