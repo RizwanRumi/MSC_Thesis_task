ad75c2f04dff234e5e0dff1e16e1b6a726382bbb
RizwanRumi <rizwan.cse33@gmail.com>
2022-11-06 18:29:52 +0100

    update delete images with labels function

--- Duplicate_Detector/duplicate_image_detector.py
+++ Duplicate_Detector/duplicate_image_detector.py
@@ -8,12 +8,14 @@ import matplotlib.pyplot as plt
 from pathlib import Path
 from datetime import datetime
 
+
 # Function that processes the directories that were input as parameters
 def check_directory(directory):
     directory = Path(directory)
     if not os.path.isdir(directory):
         raise FileNotFoundError(f"Directory " + str(directory) + " does not exist")
     return directory
+
 
 # Function that displays a progress bar during the search
 def show_progress(count, list, task='processing images'):
@@ -23,6 +25,7 @@ def show_progress(count, list, task='processing images'):
     else:
         print(f"..... {task}: [{count}/{len(list)}] [{count / len(list):.0%}]", end="\r")
 
+
 # Function that check the respective MSE value
 def map_similarity(similarity):
     try:
@@ -30,7 +33,7 @@ def map_similarity(similarity):
         ref = similarity
     except:
         if similarity == "low":
-            ref = 466
+            ref = 470
         # search for exact duplicate images, extremely sensitive, MSE < 0.1
         elif similarity == "high":
             ref = 0.1
@@ -38,6 +41,7 @@ def map_similarity(similarity):
         else:
             ref = 200
     return ref
+
 
 # Function that creates a list of matrices for each image found in the folder
 def create_imgs_matrix(directory, px_size):
@@ -68,16 +72,19 @@ def create_imgs_matrix(directory, px_size):
 
     return imgs_matrix, folder_files
 
+"""
 # Function for rotating an image matrix by a 90 degree angle
 def rotate_img(image):
     image = np.rot90(image, k=1, axes=(0, 1))
     return image
+"""
 
 # Function that calculates the mean squared error (mse) between two image matrices
 def mse(imageA, imageB):
     err = np.sum((imageA.astype("float") - imageB.astype("float")) ** 2)
     err /= float(imageA.shape[0] * imageA.shape[1])
     return err
+
 
 # Function that plots two compared image files and their mse
 def show_img_figs(imageA, imageB, err):
@@ -101,9 +108,9 @@ def show_file_info(imageA, imageB):
     imageB = "..." + str(imageB)[-45:]
     print(f"""Duplicate files:\n{imageA} and \n{imageB}\n""")
 
+
 # Function that searches one directory for duplicate/similar images
 def search_one_dir(img_matrices_A, folderfiles_A, similarity, show_output=False):
-
     result = {}
     lower_quality = []
     ref = similarity
@@ -116,35 +123,27 @@ def search_one_dir(img_matrices_A, folderfiles_A, similarity, show_output=False)
         show_progress(count_A, img_matrices_A, task='comparing images')
         for count_B, imageMatrix_B in enumerate(img_matrices_A):
             if count_B > count_A != len(img_matrices_A):
-                rotations = 0
-                while rotations <= 3:
-                    if rotations != 0:
-                        imageMatrix_B = rotate_img(imageMatrix_B)
-
-                    err = mse(imageMatrix_A, imageMatrix_B)
-                    if err < ref:
-                        if show_output:
-                            show_img_figs(imageMatrix_A, imageMatrix_B, err)
-                            show_file_info(Path(folderfiles_A[count_A][0]) / folderfiles_A[count_A][1],
-                                                # 0 is the path, 1 is the filename
-                                                Path(folderfiles_A[count_B][0]) / folderfiles_A[count_B][1])
-                        if img_id in result.keys():
-                            result[img_id]["duplicates"] = result[img_id]["duplicates"] + [
-                                str(Path(folderfiles_A[count_B][0]) / folderfiles_A[count_B][1])]
-                        else:
-                            result[img_id] = {'filename': str(folderfiles_A[count_A][1]),
-                                              'location': str(
-                                                  Path(folderfiles_A[count_A][0]) / folderfiles_A[count_A][1]),
-                                              'duplicates': [
-                                                  str(Path(folderfiles_A[count_B][0]) / folderfiles_A[count_B][1])]}
-                        break
+                err = mse(imageMatrix_A, imageMatrix_B)
+                if err < ref:
+                    if show_output:
+                        show_img_figs(imageMatrix_A, imageMatrix_B, err)
+                        # 0 is the path, 1 is the filename
+                        show_file_info(Path(folderfiles_A[count_A][0]) / folderfiles_A[count_A][1],
+                                       Path(folderfiles_A[count_B][0]) / folderfiles_A[count_B][1])
+                    if img_id in result.keys():
+                        result[img_id]["duplicates"] = result[img_id]["duplicates"] + [
+                            #  with path: str(Path(folderfiles_A[count_B][0]) / folderfiles_A[count_B][1])
+                            str(folderfiles_A[count_B][1])]
                     else:
-                        rotations += 1
-
-    result = collections.OrderedDict(sorted(result.items()))
+                        result[img_id] = {'filename': str(folderfiles_A[count_A][1]),
+                                          'location': str(Path(folderfiles_A[count_A][0]) / folderfiles_A[count_A][1]),
+                                          'duplicates': [
+                                              #  with path: str(Path(folderfiles_A[count_B][0]) / folderfiles_A[count_B][1])
+                                              str(folderfiles_A[count_B][1])]}
+    result = collections.OrderedDict(result.items())
     return result
 
-
+"""
 # Function for deleting the lower quality images that were found after the search
 def delete_images(image_list):
     deleted = 0
@@ -158,25 +157,50 @@ def delete_images(image_list):
         except:
             print("Could not delete file:", file, end="\r")
     print("\n***\nDeleted", deleted, "images.")
+"""
+
+# Function for deleting the lower quality images with labels that were found after the search
+def delete_images_with_lables(image_directory, label_directory, image_list):
+    deleted = 0
+    # delete lower quality images
+    for file in image_list:
+        print("\nDeletion in progress...", end="\r")
+        # get filename
+        name, ext = os.path.splitext(file)
+        label = str(name + '.txt')
+        image_path = Path(image_directory / file)
+        label_path = Path(label_directory / label)
+        try:
+            os.remove(image_path)
+            os.remove(label_path)
+            print("Deleted file: {0}, {1}".format(file, label), end="\r")
+            deleted += 1
+        except:
+            print("Can not Delete : {0}, {1}".format(file, label), end="\r")
+    print("\n***\nDeleted", deleted, "images.")
 
 
 if __name__ == "__main__":
 
     start_time = time.time()
-    folder_path = "E:/Thesis_Task/Duplicate_Detector/Datasets/Broken_grey_test/"
+    folder_path = "E:/Thesis_Task/Duplicate_Detector/Datasets/augmented/raw_flip_rotation_2x/train/"
     directory = check_directory(folder_path)
-    img_matrices, files = create_imgs_matrix(directory, px_size=100)
+    folders = os.listdir(directory)
+    images_directory = Path(directory / folders[0])
+    labels_directory = Path(directory / folders[1])
+
+    img_matrices, files = create_imgs_matrix(images_directory, px_size=100)
     ref = map_similarity(similarity="low")
     duplicate_result = search_one_dir(img_matrices, files, ref, show_output=False)
     end_time = time.time()
     time_elapsed = np.round(end_time - start_time, 4)
 
     duplicate_images_list = set()
-    #x = duplicate_result.keys()
-    #print(x)
+    # x = duplicate_result.keys()
+    # print(x)
     for res in duplicate_result.values():
-        #print("res: ", res)
-        for x,y in res.items():
+        # print("res: ", res)
+        for x, y in res.items():
             print(x, ':', y)
         for lst in res.get("duplicates"):
             duplicate_images_list.add(lst)
@@ -185,13 +209,14 @@ if __name__ == "__main__":
 
     images = 'image' if total_duplicate == 1 else 'images'
 
-    print("\nFound", total_duplicate, images, "with one or more duplicate/similar images in", time_elapsed, "seconds.\n")
+    print("\nFound", total_duplicate, images, "with one or more duplicate/similar images in", time_elapsed,
+          "seconds.\n")
 
     if total_duplicate != 0:
         # optional delete images
-        usr = input("Are you sure you want to delete all lower resolution and duplicate images?"
+        usr = input("Are you sure you want to delete all duplicate images with their labels?"
                     "\nThis cannot be undone. (y/n) ")
         if str(usr) == "y":
-            delete_images(list(duplicate_images_list))
+            delete_images_with_lables(images_directory, labels_directory, list(duplicate_images_list))
         else:
             print("Image deletion canceled.")
